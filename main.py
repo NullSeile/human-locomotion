7414e2b1e35c96c0ebea99b9501a854565a1284e
nullptr <eliesbertran@gmail.com>
2022-02-24 17:21:54 +0100

    Cleaned up the code and renamed stuff to make it more readable (hopefully)

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from math import *
 import pygame
 from pygame.locals import *
 import sys
 from typing import Callable, Tuple
+from math import *
 
 class Point:
 	def __init__(self, x, y):
@@ -10,13 +10,14 @@ class Point:
 		self.y = y
 
 class Bone:
-	def __init__(self, name: str, length: float, func: Callable[[float], float], children: list = []):
+	def __init__(self, name: str, length: float, function: Callable[[float], float], children: list):
 		self.name = name
-		self.l = length
-		self.c = children
-		self.f = func
-		self.r = 0
+		self.length = length
+		self.children = children
+		self.function = function
+		self.rotation = 0
 
+# Maps a float x, which goes from x0 to x1, to go from y0 to y1
 def map(x: float, x0: float, x1: float, y0: float, y1: float):
 	return y0 + ((y1 - y0) / (x1 - x0)) * (x - x0)
 
@@ -31,33 +32,35 @@ def to_screen_pos(pos: Point, center: Point, radius: float, screen: pygame.Surfa
 	
 	return x, y
 
-def update_bone(b: Bone, t: float):
-	b.r = b.f(t)
+def update_bone(bone: Bone, t: float):
+	bone.rotation = bone.function(t)
 	
-	for c in b.c:
-		update_bone(c, t)
+	for bone_child in bone.children:
+		update_bone(bone_child, t)
 
-def draw_bone(b: Bone, pos: Point, rot: float, screen: pygame.Surface):
-	rot += b.r
-	next_pos = Point(pos.x + b.l * cos(rot), pos.y + b.l * sin(rot))
+center = Point(0, 0)
+radius = 1.5
+def draw_bone(bone: Bone, pos: Point, parent_rotation: float, screen: pygame.Surface):
+	rotation = parent_rotation + bone.rotation
+	next_pos = Point(pos.x + bone.length * cos(rotation), pos.y + bone.length * sin(rotation))
 	
 	pygame.draw.line(screen, (255, 255, 255),
-						  to_screen_pos(pos, Point(0, 0), 1.5, screen),
-						  to_screen_pos(next_pos, Point(0, 0), 1.5, screen), 5)
+						  to_screen_pos(pos, center, radius, screen),
+						  to_screen_pos(next_pos, center, radius, screen), 5)
 	
-	for c in b.c:
-		draw_bone(c, next_pos, rot, screen)
+	for bone_child in bone.children:
+		draw_bone(bone_child, next_pos, rotation, screen)
 
-r = \
-	Bone("root", 0, lambda t: 0, [
+pelvis = \
+	Bone("pelvis", 0, lambda t: 0, [
 		Bone("femur_l", 0.5, lambda t: -0.4, [
-			Bone("tibia_l", 0.4, lambda t: 0.2, [])
+			Bone("tibia_l", 0.4, lambda t: 0.2 + 0.5*t, [])
 		]),
 		Bone("femur_r", 0.5, lambda t: 0.5 + 0.3*cos(3*t), [
 			Bone("tibia_r", 0.4, lambda t: -0.2 - 0.1*cos(3*t), [])
 		]),
 		Bone("spine", 0.7, lambda t: -pi, [
-			Bone("head", 0.1, lambda t: 0.1*cos(8*t), []),
+			Bone("head", 0.1, lambda t: 0.2*cos(8*t), []),
 			Bone("upper_arm_l", 0.4, lambda t: pi + 0.5 + 0.1*cos(5*t), [
 				Bone("lower_arm_l", 0.4, lambda t: -0.3 + 0.2*cos(5*t), [])
 			]),
@@ -81,10 +84,9 @@ while True:
 	
 	screen.fill((0, 0, 0))
 	
-	update_bone(r, t)
-	draw_bone(r, Point(0, 0), pi/2, screen)
+	update_bone(pelvis, t)
+	draw_bone(pelvis, Point(0, 0), pi/2, screen)
 	
 	pygame.display.update()
 	
 	t += clock.tick(60) / 1000
-	
